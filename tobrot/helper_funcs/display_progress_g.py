8ded464a733226eab9ebe88cbb69994bf4603adf
AD <66907615+add830830@users.noreply.github.com>
2020-07-29 21:46:39 +0530

    Update display_progress_g.py

--- tobrot/helper_funcs/display_progress_g.py
+++ tobrot/helper_funcs/display_progress_g.py
@@ -37,11 +37,11 @@ async def progress_for_pyrogram_g(
         estimated_total_time = time_formatter(milliseconds=estimated_total_time)
 
         progress = "║ {0}{1} ║ \nPercentage • {2}%\n".format(
-            ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]),
-            ''.join([UN_FINISHED_PROGRESS_STR for i in range(10 - math.floor(percentage / 10))]),
+            ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 4))]),
+            ''.join([UN_FINISHED_PROGRESS_STR for i in range(25 - math.floor(percentage / 4))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\nSpeed • {2}/sec\nETA • {3}\n".format(
+        tmp = progress + "{0}/{1}\nSpeed • {2}/sec\nETA • {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
