ae1b6aa3dda41409281649704bbc73f62e7959d9
AD <66907615+add830830@users.noreply.github.com>
2020-08-11 22:34:21 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -34,7 +34,7 @@ async def help_message_f(client, message):
 async def rename_message_f(client, message):
     await message.reply_photo(
                 #text_message,
-                photo='https://grouphelp.top/channel/api/beta/?file_id=AgACAgUAAxkBAAIhcF8m51XiPprSWKU3GBOaUoIEHFoVAAJqqjEbo6_oVmYG0Q-TbGWeug8ia3QAAwEAAwIAA3kAA8GeAQABGgQ',
+                photo="https://grouphelp.top/channel/api/beta/?file_id=AgACAgUAAxkBAAIhcF8m51XiPprSWKU3GBOaUoIEHFoVAAJqqjEbo6_oVmYG0Q-TbGWeug8ia3QAAwEAAwIAA3kAA8GeAQABGgQ",
                 quote=True,
                 caption=text_message,
                 reply_markup=reply_markup
